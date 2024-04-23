a482ddfe2f62a50ed1e9a7c21b1bc0a0aa132dcd
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-04-23 10:04:41 +0200

    WIP (github API)

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,9 @@
 import MapRoutes from "./MapRoutes.js";
 
 export default function (app) {
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
   new MapRoutes(app);
   // Add new routes here
 }
